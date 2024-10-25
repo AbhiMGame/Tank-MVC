b8b920148c4a9b0334a6c92a122551353aa4d808
MoonJSonic <abhijeet.munje@gmail.com>
2024-10-25 13:09:18 +0530

    Implemented Movement & Rotation

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,17 +6,25 @@ public class TankController
 {
     private TankModel tankModel;
     private TankView tankView;
+    private Rigidbody rb;
     public TankController(TankModel _tankModel,TankView _tankView)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
-
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetRigidbody();
         tankModel.SetTankController(this);
         tankView.SetTankController(this);   
 
-        GameObject.Instantiate(tankView.gameObject);
-
+    }
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity=tankView.transform.forward * movement * movementSpeed;
     }
 
-
+    public void Rotate(float roatate,float rotateSpeed)
+    {
+        Vector3 vector = new Vector3(0f, roatate * rotateSpeed, 0f);
+        Quaternion deltaRoatation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRoatation);
+    }
 }
